d9b0c68ed2fa957d9058c295884928fce6138334
vitorpedeo <vitorpereira@discente.ufg.br>
2020-01-24 05:53:35 -0300

    Atividade Sala Consertada

--- poo20204_201907515_vitor/src/aula06/salaaula/endereco/Teste.java
+++ poo20204_201907515_vitor/src/aula06/salaaula/endereco/Teste.java
@@ -1,14 +1,13 @@
 package aula06.salaaula.endereco;
 
-//Por algum motivo não está funcionando, consertar!
 public class Teste {
     public static void main(String[] args) {
         Pais p = new Pais("Brasil");
         Estado e = new Estado("Goiás", "GO", p);
-        Municipio mun = new Municipio(25300L, "Goiânia", e);
+        Municipio mun = new Municipio(25300l, "Goiânia", e);
         Bairro b = new Bairro("Setor Central", "Central", mun);
-        Logradouro log = new Logradouro("Paranaíba", TipoLogradouro.AVENIDA);
-        Endereco end = new Endereco("125", "QD.45 LT.28 26A", 756692100, "Residencial", b, log);
+        Logradouro l = new Logradouro("Joaquim Lúcio", TipoLogradouro.TRAVESSIA);
+        Endereco end = new Endereco("25", "Qd.35 Lt6 AS9", 45850000, "Residencial", b, l);
 
         System.out.println(end);
     }
